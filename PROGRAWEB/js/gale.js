1fb504a8614f1b0f4ba5f876869c349bc1fa589c
Alberto-K <albertocastillop@outlok.com.com>
2021-01-11 23:01:02 -0600

    Nevado1

--- PROGRAWEB/js/gale.js
+++ PROGRAWEB/js/gale.js
@@ -1,7 +1,8 @@
-var rootref = database.ref().child("Image");
+var rootref = database.ref().child("Nevado");
 
 rootref.on("child_added", snap => {
-var image = snap.child("img").val();
+var image = snap.child("foto").val();
 
-$("#gallery_div").append("<img src="+ image + "></img>");
+$("#gallery_div").append("<div class='col-md-4 col-xs-6 thumb'><a class='thumbnail' href= "+image+" ><img class='img-responsive' src= "+ image + " alt=''></a>");
+
 }); 
